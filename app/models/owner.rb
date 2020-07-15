ea9a676c161bb0acc10ba29eefd6672b0fb9d6e4
Kathy Bui <kathy.bui1992@gmail.com>
2020-07-14 20:58:28 -0600

    Finsih all of 4owner_spec

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -2,7 +2,13 @@ class Owner < ApplicationRecord
   has_many :ownerships
   has_many :dogs, through: :ownerships
 
-  # def all_dog_names
-  #   Dog.order(:name)
-  # end
+  def self.all_dog_names
+    # select dogs.name from owners join ownerships on owners.id = ownerships.owner_id join dogs on ownerships.dog_id = dogs.id order by name;
+    Owner.joins(:dogs).pluck('dogs.name')
+  end
+
+  def self.unique_dog_names
+    # select distinct dogs.name from owners join ownerships on owners.id = ownerships.owner_id join dogs on ownerships.dog_id = dogs.id order by name desc;
+    Owner.joins(:dogs).pluck('dogs.name').sort.reverse.uniq
+  end
 end
