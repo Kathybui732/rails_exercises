3c13de91c9e97f248b16744efc4fa59a22205ef1
Kathy Bui <kathy.bui1992@gmail.com>
2020-07-14 21:26:04 -0600

    Finish 1restful_spec

---
+++ app/controllers/dogs_controller.rb
@@ -0,0 +1,40 @@
+class DogsController < ApplicationController
+
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    dog = Dog.create(dog_params)
+    if dog.save
+      redirect_to "/dogs"
+    end
+  end
+
+  def edit
+    @dog = Dog.find(params[:id])
+  end
+
+  def update
+    dog = Dog.find(params[:id])
+    dog.update(dog_params)
+    redirect_to "/dogs"
+  end
+
+  def destroy
+    Dog.destroy(params[:id])
+    redirect_to "/dogs"
+  end
+
+  private
+  def dog_params
+    params.permit(:name, :age, :breed)
+  end
+end
