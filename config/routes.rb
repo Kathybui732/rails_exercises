e79e050402c04703a0d67d4d30b8aeb6800d2bc3
Kathy Bui <kathy.bui1992@gmail.com>
2020-07-15 20:07:59 -0600

    finish exercise 4

--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,20 @@ Rails.application.routes.draw do
   patch '/dogs/:id', to: 'dogs#update'
   delete '/dogs/:id', to: 'dogs#destroy'
 
+  # nested routing
   get '/dogs/:dog_id/tricks', to: 'tricks#index'
   get '/dogs/:dog_id/tricks/new', to: 'tricks#new'
   post '/dogs/:dog_id/tricks', to: 'tricks#create'
 
-  #Non RESTful routes
+  # Non RESTful routes
   get '/hello', to: 'nonrestful#hello'
   get '/greetings/:name', to: 'nonrestful#what_is_your_name'
   patch '/items/:the_item_id/deactivate', to: 'nonrestful#deactivate'
   delete '/subtract/:x/from/:y', to: 'nonrestful#subtract'
+
+  # namespaced routing
+  get 'admin/users/:id', to: 'admin/users#show'
+  get 'admin/dogs', to: 'admin/dogs#index'
+  get '/admin/dogs/:dog_id/tricks', to: 'admin/tricks#index'
+  delete '/admin/tricks/:id', to: 'admin/tricks#destroy'
 end
